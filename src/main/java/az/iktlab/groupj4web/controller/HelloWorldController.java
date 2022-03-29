d3829819959807b18038f56b83a20a8fbe3f7025
Khayal Farziyev <khayal.farziyev@pashabank.az>
2022-03-29 09:43:38 -0700

    http

--- src/main/java/az/iktlab/groupj4web/controller/HelloWorldController.java
+++ src/main/java/az/iktlab/groupj4web/controller/HelloWorldController.java
@@ -1,8 +1,6 @@
 package az.iktlab.groupj4web.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -11,5 +9,25 @@ public class HelloWorldController {
     @GetMapping
     public String hello() {
         return "<h1> Hello Spring Boot World </h1>";
+    }
+
+    @PostMapping
+    public void postEx() {
+        System.out.println("Post mapping example");
+    }
+
+    @PutMapping
+    public void putEx() {
+        System.out.println("Put mapping example");
+    }
+
+    @PatchMapping
+    public void patchEx() {
+        System.out.println("Patch mapping example");
+    }
+
+    @DeleteMapping
+    public void deleteEx() {
+        System.out.println("Delete mapping example");
     }
 }
